35af3b8f4ede922a105e0a534d259bb2a09b2302
Gerardo2006 <164081228+Gerardo2006@users.noreply.github.com>
2024-05-15 14:00:21 -0600

    Ejercicios 15 mayo

--- 15 de mayo/Estadio XYZ.cpp
+++ 15 de mayo/Estadio XYZ.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main()
 {
-    int sec, total, solg = 3, solp = 5, som = 8, tri = 15, pla = 20;
+    int sec, solg = 3, solp = 5, som = 8, tri = 15, pla = 20;
 
     cout << "1) sol general\n";
     cout << "2) sol preferencial\n";
@@ -17,20 +17,43 @@ int main()
     switch(sec)
     {
         case 1:
-        int cant;
+        int a;
         cout << "Ingrese la cantidad de entradas que quiere en sol general: \n";
-        cin >> cant;
-        cout << "Selecciono " << cant << " entradas por un precio de " << cant * solg << "$";
+        cin >> a;
+        cout << "Selecciono " << a << " entradas por un precio de " << a * solg << "$";
         break;
 
         case 2:
-        int cant;
+        int b;
         cout << "Ingrese la cantidad de entradas que quiere en sol preferencial: \n";
-        cin >> cant;
-        cout << "Selecciono " << cant << " entradas por un precio de " << cant * solp << "$";
+        cin >> b;
+        cout << "Selecciono " << b << " entradas por un precio de " << b * solp << "$";
         break;
 
         case 3:
-        
+        int c;
+        cout << "Ingrese la cantidad deentradas que quiere en sombra: \n";
+        cin >> c;
+        cout << "Selecciono " << c << " entradas por un precio de " << c * som << "$";
+        break;
+
+        case 4:
+        int d;
+        cout << "Ingrese la cantidad de entradas que quiere en tirbuna: \n";
+        cin >> d;
+        cout << "Selecciono " << d << " entradas por un precio de " << d * tri << "$";
+        break;
+
+        case 5:
+        int e;
+        cout << "Ingrese la cantidad de entradas que quiere en platea: \n";
+        cin >> e;
+        cout << "Selecciono " << e << " entradas por un precio de " << e * pla << "$";
+        break;
+
+        default:
+        cout << "No valor no valido";
+        break;
     }
+    return 0;
 }
