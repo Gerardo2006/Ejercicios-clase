20a7ff4d0061d543fda90331b6650506f3af9a99
Gerardo2006 <164081228+Gerardo2006@users.noreply.github.com>
2024-06-03 12:34:37 -0600

    3 de junio

--- 3 de Junio/Ejercicio1.cpp
+++ 3 de Junio/Ejercicio1.cpp
@@ -29,13 +29,20 @@ float division(float entero1, float entero2)
     return resultado;
 }
 
+double media(double entero1, double entero2)
+{
+    double media = 0;
+    media = (entero1 + entero2) / 2;
+    return media;
+}
+
 int main()
 {
     float n1 = 0, n2 = 0, respuesta = 0;
     n1 = 8;
     n2 = 6;
 
-    respuesta = division(n1, n2);
+    respuesta = media(n1, n2);
     cout << "El valor de la suma de dos numeros enteros es: " << respuesta;
 
     return 0;
